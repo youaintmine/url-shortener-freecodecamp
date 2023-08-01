f69e031b6bd291bb4a2bdd8c54433f6ade95f1bf
youaintmine <ahmbarishsaikia103@gmail.com>
2023-08-01 16:51:18 +0530

    Added commits

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const dns = require('dns');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -10,8 +11,7 @@ let urlList = [];
 
 
 app.use(cors());
-app.use(bodyParser);
-app.use(bodyParser.urlencoded);
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -24,9 +24,40 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl', (req, res) => {
-  console.log(req.body);
+app.post('/api/shorturl/:id?', (req, res) => {
+
+  const id = req.params.id;
+
+  if(id){
+    //check
+    if(id < urlList.length()){
+      return res.json({
+        error: 'invalid url'
+      });
+    }else {
+      const url = urlList[id-1];
+      return res.redirect(url);
+    }
+  }
+
+
+  const inputUrl = req.body['url'];
+
+  dns.lookup(inputUrl, (err, address, family) => {
+    if(err){
+      return res.json({ error: 'invalid url' });
+    }else{
+      urlList.push(inputUrl);
+      return res.json({
+        original_url : inputUrl,
+        short_url : urlList.length()
+      });
+    }
+  });
+  
 });
+
+app.get('/api/shorturl/:')
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
